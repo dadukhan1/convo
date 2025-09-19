06fe707abefad2a1774ea3873c2c95897db31f96
Dadu Khan <photoshopt65@gmail.com>
2025-09-19 17:51:49 +0500

    chatpage ui setup

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,47 @@
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
+import { toast } from 'react-hot-toast';
+
+
+export const useChatStore = create((set, get) => ({
+    allContacts: [],
+    chats: [],
+    messages: [],
+    activeTab: "chats",
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    isSoundEnabled: localStorage.getItem("isSoundEnabled") === "true",
+
+    toggleSound: () => {
+        const current = get().isSoundEnabled;
+        localStorage.setItem("isSoundEnabled", !current);
+        set({ isSoundEnabled: !current });
+    },
+
+    setActiveTab: (tab) => set({ activeTab: tab }),
+    setSelectedUser: (selectedUser) => set({ selectedUser }),
+
+    getAllContacts: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/contacts");
+            set({ allContacts: res.data });
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Something went wrong");
+        } finally {
+            set({ isUsersLoading: false });
+        }
+    },
+    getMyChatPartners: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/chats");
+            set({ chats: res.data });
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Something went wrong");
+        } finally {
+            set({ isUsersLoading: false });
+        }
+    },
+}))
